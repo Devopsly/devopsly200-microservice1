0e716c5c8914dd633388dd5954e8f6d8b32b8122
ramesh <ramesh@nirvaha.com>
2017-01-03 14:24:26 -0800

    misc

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
-ls
-startingPort=8000
-buildNumber=${BUILD_NUMBER}
-port=$((startingPort + buildNumber))
-echo "var port= {};port.number = $port;module.exports = port;" > port.js
+#startingPort=8000
+#buildNumber=${BUILD_NUMBER}
+#port=$((startingPort + buildNumber))
+#echo "var port= {};port.number = $port;module.exports = port;" > port.js
 
 docker rmi -f localhost:5000/devopsly200-microservice1:working
 docker build -t localhost:5000/devopsly200-microservice1:working .
