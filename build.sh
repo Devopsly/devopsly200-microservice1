f105df43139e9c6e0a600668024ccb978d603058
ramesh <ramesh@nirvaha.com>
2017-01-13 10:14:14 -0800

    debug build.sh, removed tags

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 buildNumber=${BUILD_NUMBER}
 
-docker rmi -f localhost:5000/devopsly200-microservice1-teststage:$buildNumber
-docker build -t localhost:5000/devopsly200-microservice1-teststage:$buildNumber -f DockerfileTest .
+docker rmi -f localhost:5000/devopsly200-microservice1-teststage
+docker build -t localhost:5000/devopsly200-microservice1-teststage -f DockerfileTest .
 
-docker rmi -f localhost:5000/devopsly200-microservice1-teststage-failed:$buildNumber
-docker build -t localhost:5000/devopsly200-microservice1-teststage-failed:$buildNumber -f DockerfileTest .
+docker rmi -f localhost:5000/devopsly200-microservice1-teststage-failed
+docker build -t localhost:5000/devopsly200-microservice1-teststage-failed -f DockerfileTest .
 
-docker rmi -f localhost:5000/devopsly200-microservice1-stagingstage:$buildNumber
-docker build -t localhost:5000/devopsly200-microservice1-stagingstage:$buildNumber -f DockerfileStaging .
+docker rmi -f localhost:5000/devopsly200-microservice1-stagingstage
+docker build -t localhost:5000/devopsly200-microservice1-stagingstage -f DockerfileStaging .
 
-docker rmi -f localhost:5000/devopsly200-microservice1-stagingstage-failed:$buildNumber
-docker build -t localhost:5000/devopsly200-microservice1-stagingstage-failed:$buildNumber -f DockerfileStaging .
+docker rmi -f localhost:5000/devopsly200-microservice1-stagingstage-failed
+docker build -t localhost:5000/devopsly200-microservice1-stagingstage-failed -f DockerfileStaging .
 
-docker rmi -f localhost:5000/devopsly200-microservice1-productionstage:$buildNumber
-docker build -t localhost:5000/devopsly200-microservice1-productionstage:$buildNumber -f DockerfileProduction .
+docker rmi -f localhost:5000/devopsly200-microservice1-productionstage
+docker build -t localhost:5000/devopsly200-microservice1-productionstage -f DockerfileProduction .
 
-docker rmi -f localhost:5000/devopsly200-microservice1-productionstage-failed:$buildNumber
-docker build -t localhost:5000/devopsly200-microservice1-productionstage-failed:$buildNumber -f DockerfileProduction .
+docker rmi -f localhost:5000/devopsly200-microservice1-productionstage-failed
+docker build -t localhost:5000/devopsly200-microservice1-productionstage-failed -f DockerfileProduction .
--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 buildNumber=${BUILD_NUMBER}
 
-docker push localhost:5000/devopsly200-microservice1-teststage:$buildNumber
-docker push localhost:5000/devopsly200-microservice1-teststage-failed:$buildNumber
-docker push localhost:5000/devopsly200-microservice1-stagingstage:$buildNumber
-docker push localhost:5000/devopsly200-microservice1-stagingstage-failed:$buildNumber
-docker push localhost:5000/devopsly200-microservice1-productionstage:$buildNumber
-docker push localhost:5000/devopsly200-microservice1-productionstage-failed:$buildNumber
+docker push localhost:5000/devopsly200-microservice1-teststage
+docker push localhost:5000/devopsly200-microservice1-teststage-failed
+docker push localhost:5000/devopsly200-microservice1-stagingstage
+docker push localhost:5000/devopsly200-microservice1-stagingstage-failed
+docker push localhost:5000/devopsly200-microservice1-productionstage
+docker push localhost:5000/devopsly200-microservice1-productionstage-failed
